bb1320c535b0f169caad578feefee06b609fc2a4
Kayla Blalock <kay.blalock91@gmail.com>
2020-07-16 22:45:55 -0400

    Use jquery

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 var currentDate = moment().format("dddd LL");
 
 $("#currentDay").text(currentDate);
+
+function createTimeblock() {
+    // need time
+    var hour = $("<div>").addClass("hour")
+    // need description box
+    var description = $("<div>").addClass("description")
+    // need save button
+    var saveBTN = $("<div>").addClass("saveBTN")
+
+    var row = $("<div>").addClass("row")
+}
